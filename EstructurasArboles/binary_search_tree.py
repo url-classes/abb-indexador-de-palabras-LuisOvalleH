0cd62fcce34c70fb27733c6d3572ef82ab44b71c
Massy100 <massielle.coti@gmail.com>
2024-04-18 10:43:58 -0600

    arbol de busqueda graficado y con diccionario de palabras

--- EstructurasArboles/binary_search_tree.py
+++ EstructurasArboles/binary_search_tree.py
@@ -1,58 +1,47 @@
+import tkinter as tk
+from tkinter import filedialog, Canvas
+import os
+import shutil
 from EstructurasArboles.node import Node
-from typing import  TypeVar
+import re
+from typing import Optional, TypeVar, Generic
+
 T = TypeVar("T")
+
 class BinarySearchTree:
     def __init__(self):
-        self.root: Node[T] | None = None
+        self.root = None
         
+    # FUNCION  PARA CALCULAR LA ALTURA DEL ARBOl
+    def altura(self,subtree: Node[T]):
+
+        if subtree is None:
+            return 0
+
+        return 1 + max(self.altura(subtree.left), self.altura(subtree.right))
+
     def insert(self, root, data):
         if root is None:
-            return Node(data)
+            return Node(data)  # Crea un nodo si el dato no existe en el árbol
         else:
             if data < root.data:
                 root.left = self.insert(root.left, data)
-            else:
+            elif data > root.data:
                 root.right = self.insert(root.right, data)
-        return root
+        return root  # Si la palabra ya existe, no hace nada y retorna el nodo sin cambios
 
-    def search(self, root,data):
-        if root is None or root.val == data:
-            return root
-        if root.data < data:
-            return self.search(root.right, data)
-        return self.search(root.left, data)
+    def draw_tree(self, root, canvas, x, y, step=0):
+        step = 35 * self.altura(root)
+        if root is not None:
+            node_radius = 20 
+            canvas.create_oval(x - node_radius, y - node_radius, x + node_radius, y + node_radius, fill="white")
+            canvas.create_text(x, y, text=root.data, font=('Helvetica', '9')) 
+            new_step = step + 40
+            if root.left is not None:
+                canvas.create_line(x - node_radius, y + node_radius, x - step, y + 100, arrow=tk.LAST)
+                self.draw_tree(root.left, canvas, x - step, y + 100, step * 0.5)
+            if root.right is not None:
+                canvas.create_line(x + node_radius, y + node_radius, x + step, y + 100, arrow=tk.LAST)
+                self.draw_tree(root.right, canvas, x + step, y + 100, step * 0.5)
 
-    def delete(self, root, data):
-        if root is None:
-            return root
-        if data < root.data:
-            root.left = self.delete(root.left, data)
-        elif data > root.val:
-            root.right = self.delete(root.right, data)
-        else:
-            if root.left is None:
-                temp = root.right
-                root = None
-                return temp
-            elif root.right is None:
-                temp = root.left
-                root = None
-                return temp
-            temp = self.min_value_node(root.right)
-            root.data = temp.val
-            root.data = self.delete(root.right, temp.data)
-        return root
 
-    def min_value_node(self, node):
-        current = node
-        while current.left is not None:
-            current = current.left
-        return current
-
-    def inorder_traversal(self, root):
-        result = []
-        if root:
-            result = self.inorder_traversal(root.left)
-            result.append(root.data)
-            result += self.inorder_traversal(root.right)
-        return result
--- main.py
+++ main.py
@@ -1,1 +1,83 @@
-print('hola  mundo')
+import tkinter as tk
+from tkinter import filedialog, Canvas, Scrollbar, Frame
+from EstructurasArboles.binary_search_tree import BinarySearchTree
+import os
+import shutil
+import re
+
+def cargar_archivo():
+    # Directorio para guardar los archivos
+    directorio = 'archivos'
+    if not os.path.exists(directorio):
+        os.makedirs(directorio)
+
+    # Abre un cuadro de diálogo para seleccionar un archivo .txt
+    filepath = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if filepath:
+        # Copia el archivo al directorio 'archivos'
+        destino = os.path.join(directorio, os.path.basename(filepath))
+        shutil.copy(filepath, destino)
+        print(f"Archivo guardado en: {destino}")
+    else:
+        print("No se seleccionó ningún archivo")
+        
+def crear_arbol():
+    directorio = 'archivos'
+    bst = BinarySearchTree()
+    palabra_dict = {}
+    for filename in os.listdir(directorio):
+        if filename.endswith('.txt'):
+            with open(os.path.join(directorio, filename), 'r', encoding='utf-8') as file:
+                texto = file.read()
+                palabras = re.findall(r'\b\w+\b', texto)
+                for palabra in palabras:
+                    palabra = palabra.lower()
+                    if palabra not in palabra_dict:
+                        palabra_dict[palabra] = 1
+                    else:
+                        palabra_dict[palabra] += 1
+                    if palabra_dict[palabra] == 1:  # Inserta solo si es la primera vez
+                        bst.root = bst.insert(bst.root, palabra)
+    conteo_palabras = ["Conteo de palabras:"]
+    counter = 0
+    for k, v in palabra_dict.items():
+        conteo_palabras.append(f"{k}: {v}")
+        counter += 1
+        if counter % 10 == 0:
+            conteo_palabras.append("\n")  # Inserta un salto de línea cada 10 palabras
+    label_text.set(" ".join(conteo_palabras))  # Usa espacios para unir y mantener el formato en el label
+    # Crear ventana para el canvas con barras de desplazamiento
+    new_window = tk.Toplevel()
+    new_window.title("Visualización del Árbol Binario")
+    frame = Frame(new_window)
+    frame.pack(fill=tk.BOTH, expand=True)
+    canvas = Canvas(frame, bg='white', scrollregion=(0, 0, 5000, 3000))
+    hbar = Scrollbar(frame, orient=tk.HORIZONTAL)
+    hbar.pack(side=tk.BOTTOM, fill=tk.X)
+    hbar.config(command=canvas.xview)
+    vbar = Scrollbar(frame, orient=tk.VERTICAL)
+    vbar.pack(side=tk.RIGHT, fill=tk.Y)
+    vbar.config(command=canvas.yview)
+    canvas.config(width=800, height=600, xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+    canvas.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+    bst.draw_tree(bst.root, canvas, 2500, 50, 200) 
+    # Imprimir conteo de palabras
+    print("Conteo de palabras:", palabra_dict)
+
+    
+# Crea la ventana principal
+root = tk.Tk()
+root.title("Cargador de Archivos TXT")
+
+btn_cargar = tk.Button(root, text="Cargar archivo", command=cargar_archivo)
+btn_cargar.pack(pady=20)
+btn_arbol = tk.Button(root, text="Crear Arbol", command=crear_arbol)
+btn_arbol.pack(pady=20)
+
+label_text = tk.StringVar()
+word_count_label = tk.Label(root, textvariable=label_text)
+word_count_label.pack(pady=20)
+
+root.geometry('600x400')
+root.mainloop()
+
